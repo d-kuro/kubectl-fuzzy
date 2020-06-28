61745de79782eb57e11cea7e27de7b7f460d4978
d-kuro <kurosawa7620@gmail.com>
2020-06-29 02:38:36 +0900

    Add describe command

---
+++ pkg/cmd/describe.go
@@ -0,0 +1,196 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"k8s.io/cli-runtime/pkg/printers"
+
+	"k8s.io/apimachinery/pkg/api/meta"
+
+	"k8s.io/kubectl/pkg/describe"
+
+	"github.com/d-kuro/kubectl-fuzzy/pkg/fuzzyfinder"
+
+	"k8s.io/cli-runtime/pkg/resource"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+)
+
+const (
+	exampleDescribe = `
+	# Selecting a Object with the fuzzy finder and view the log and show details
+	kubectl fuzzy describe TYPE [flags]
+`
+)
+
+// NewCmdDescribe provides a cobra command wrapping DescribeOptions.
+func NewCmdDescribe(streams genericclioptions.IOStreams) *cobra.Command {
+	o := NewDescribeOptions(streams)
+
+	cmd := &cobra.Command{
+		Use:           "describe",
+		Short:         "Selecting a object with the fuzzy finder and show details",
+		Example:       exampleDescribe,
+		SilenceUsage:  true,
+		SilenceErrors: true,
+		RunE: func(c *cobra.Command, args []string) error {
+			if err := o.Complete(c, args); err != nil {
+				return err
+			}
+
+			if err := o.Validate(); err != nil {
+				return err
+			}
+
+			if err := o.Run(c.Context(), args); err != nil {
+				return err
+			}
+
+			return nil
+		},
+	}
+
+	flags := cmd.Flags()
+	o.configFlags.AddFlags(flags)
+	o.AddFlags(flags)
+
+	return cmd
+}
+
+// DescribeOptions provides information required to update
+// the current context on a user's KUBECONFIG.
+type DescribeOptions struct {
+	configFlags *genericclioptions.ConfigFlags
+	printFlags  *genericclioptions.JSONYamlPrintFlags
+	genericclioptions.IOStreams
+
+	DescriberSettings *describe.DescriberSettings
+
+	Builder   *resource.Builder
+	Describer func(*meta.RESTMapping) (describe.ResourceDescriber, error)
+
+	AllNamespaces bool
+	Namespace     string
+	Selector      string
+	BuilderArgs   []string
+
+	Preview       bool
+	PreviewFormat string
+}
+
+// AddFlags adds a flag to the flag set.
+func (o *DescribeOptions) AddFlags(flags *pflag.FlagSet) {
+	// kubectl flags
+	flags.BoolVarP(&o.AllNamespaces, "all-namespaces", "A", false,
+		"If present, list the requested object(s) across all namespaces."+
+			"Namespace in current context is ignored even if specified with --namespace.")
+	flags.StringVarP(&o.Selector, "selector", "l", "",
+		"Selector (label query) to filter on, supports '=', '==', and '!='.(e.g. -l key1=value1,key2=value2)")
+	flags.BoolVar(&o.DescriberSettings.ShowEvents, "show-events", true,
+		"If true, display events related to the described object.")
+
+	// original flags
+	flags.BoolVar(&o.Preview, "preview", false,
+		"If true, display the object YAML|JSON by preview window for fuzzy finder selector.")
+	flags.StringVar(&o.PreviewFormat, "preview-format", "yaml",
+		"Rreview window output format. One of json|yaml.")
+}
+
+// NewDescribeOptions provides an instance of DescribeOptions with default values.
+func NewDescribeOptions(streams genericclioptions.IOStreams) *DescribeOptions {
+	return &DescribeOptions{
+		configFlags: genericclioptions.NewConfigFlags(true),
+		printFlags:  genericclioptions.NewJSONYamlPrintFlags(),
+		IOStreams:   streams,
+		DescriberSettings: &describe.DescriberSettings{
+			ShowEvents: true,
+		},
+	}
+}
+
+// Complete sets all information required for show details.
+func (o *DescribeOptions) Complete(cmd *cobra.Command, args []string) error {
+	o.Builder = resource.NewBuilder(o.configFlags)
+
+	o.Describer = func(mapping *meta.RESTMapping) (describe.ResourceDescriber, error) {
+		return describe.DescriberFn(o.configFlags, mapping)
+	}
+
+	o.BuilderArgs = args
+
+	if !o.AllNamespaces {
+		kubeConfig := o.configFlags.ToRawKubeConfigLoader()
+
+		namespace, _, err := kubeConfig.Namespace()
+		if err != nil {
+			return fmt.Errorf("faild to get namespace from kube config: %w", err)
+		}
+
+		o.Namespace = namespace
+	}
+
+	o.PreviewFormat = strings.ToLower(o.PreviewFormat)
+
+	return nil
+}
+
+// Validate ensures that all required arguments and flag values are provided.
+func (DescribeOptions) Validate() error {
+	return nil
+}
+
+// Run execute fizzy finder and show details.
+func (o *DescribeOptions) Run(ctx context.Context, args []string) error {
+	r := o.Builder.
+		Unstructured().
+		ContinueOnError().
+		NamespaceParam(o.Namespace).DefaultNamespace().AllNamespaces(o.AllNamespaces).
+		LabelSelectorParam(o.Selector).
+		ResourceTypeOrNameArgs(true, o.BuilderArgs...).
+		//nolint:godox
+		SingleResourceType(). // TODO: Support multi resource type
+		Flatten().
+		Do()
+
+	if err := r.Err(); err != nil {
+		return err
+	}
+
+	infos, err := r.Infos()
+	if err != nil {
+		return err
+	}
+
+	var printer printers.ResourcePrinter
+	if o.Preview {
+		printer, err = o.printFlags.ToPrinter(o.PreviewFormat)
+		if err != nil {
+			return err
+		}
+	}
+
+	info, err := fuzzyfinder.Infos(infos, o.AllNamespaces, printer)
+	if err != nil {
+		return fmt.Errorf("failed to fuzzyfinder execute: %w", err)
+	}
+
+	mapping := info.ResourceMapping()
+
+	describer, err := o.Describer(mapping)
+	if err != nil {
+		return err
+	}
+
+	s, err := describer.Describe(info.Namespace, info.Name, *o.DescriberSettings)
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintf(o.Out, "%s", s)
+
+	return nil
+}
--- pkg/fuzzyfinder/fuzzyfinder.go
+++ pkg/fuzzyfinder/fuzzyfinder.go
@@ -1,7 +1,13 @@
 package fuzzyfinder
 
 import (
+	"bytes"
 	"fmt"
+	"strings"
+
+	"k8s.io/cli-runtime/pkg/printers"
+
+	"k8s.io/cli-runtime/pkg/resource"
 
 	"github.com/ktr0731/go-fuzzyfinder"
 	corev1 "k8s.io/api/core/v1"
@@ -32,4 +38,36 @@ func Containers(containers []corev1.Container) (corev1.Container, error) {
 	}
 
 	return containers[idx], nil
+}
+
+func Infos(infos []*resource.Info, allNamespaces bool, printer printers.ResourcePrinter) (*resource.Info, error) {
+	var opts []fuzzyfinder.Option
+
+	if printer != nil {
+		opts = append(opts, fuzzyfinder.WithPreviewWindow(func(i, w, h int) string {
+			if i >= 0 {
+				buf := &bytes.Buffer{}
+				if err := printer.PrintObj(infos[i].Object, buf); err != nil {
+					return fmt.Sprintf("preview display error: %s", err)
+				}
+				return strings.TrimPrefix(buf.String(), "---\n")
+			}
+			return ""
+		}))
+	}
+
+	idx, err := fuzzyfinder.Find(infos,
+		func(i int) string {
+			if allNamespaces {
+				return fmt.Sprintf("%s (%s)", infos[i].Name, infos[i].Namespace)
+			}
+			return infos[i].Name
+		},
+		opts...,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return infos[idx], nil
 }
