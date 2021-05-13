b4cb5c7c26d8e697ac69b38fd5efc9e099892fc2
d-kuro <kurosawa7620@gmail.com>
2021-05-13 15:34:54 +0900

    Add comments for global variable

--- pkg/cmd/version.go
+++ pkg/cmd/version.go
@@ -7,8 +7,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// Version indicates the semver of the kubectl-fuzzy command.
 const Version = "v1.9.0"
 
+// Revision indicates the revision of git where kubectl-fuzzy was built.
+// This is dynamically embedded with the -ldflags option when the binary is built.
+// The default display when not embedding is "development".
 var Revision = "development" //nolint:gochecknoglobals
 
 // NewCmdVersion is return version command.
